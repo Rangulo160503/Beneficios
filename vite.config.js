7681f520c6c58c239e05603a9f919b60036299c0
rangulo <rangulo@CR9-ITS-RANGULO>
2025-06-15 14:04:49 -0600

    chore: limpieza y preparación para despliegue Azure

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build' // ✅ Esta línea cambia la salida de Vite a /build
+  }
 })
